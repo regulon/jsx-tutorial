cb002c5eea86af784b370c6614889124b2643bba
regulon <reguloluna@gmail.com>
2023-04-13 09:12:48 -0300

    Completado el ejercicio tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: "😃",
+    name: "Cara Sonriendo"
+  },
+  {
+    emoji: "🎉",
+    name: "Cañón De Confeti"
+  },
+  {
+    emoji: "💃",
+    name: "Mujer Bailando"
+  },
+];
+
 function App() {
+  const greeting = "greeting";
+  const displayAction = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+    <h1 id={greeting}>Hello, World!</h1>
+    {displayAction && <p>I'm writing JSX</p>}
+    <ul>
+      {
+        emojis.map(emoji => (<li key={emoji.name}>
+          <button onClick={displayEmojiName}>
+            <span role='img' aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+          </button>
+        {/*<li>
+        <button onClick={displayEmojiName}>
+          <span role='img' aria-label='Cara Sonriendo' id='Cara Sonriendo'>😃</span>
+        </button>
+        </li>
+        <li>
+          <button onClick={displayEmojiName}>
+            <span role='img' aria-label='Cañón De Confeti' id='Cañón De Confeti'>🎉</span>
+          </button>
+        </li>
+        <li>
+          <button onClick={displayEmojiName}>
+            <span role='img' aria-label='Mujer Bailando' id='Mujer Bailando'>💃</span>
+          </button>
+        </li> */}
+          </li>
+        ))
+      }
+    </ul>
+  </div>
   );
 }
 
